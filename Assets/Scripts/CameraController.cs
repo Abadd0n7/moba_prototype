3de454e8b6ae2337d9abe98614902126e61f44b7
Abadd0n <lpytlak27@gmail.com>
2019-05-15 17:51:36 +0200

    Master Degree MOBA-Game Prototype

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+
+    float speed = 1000.0f;
+    //int boundary = 1;
+    //int width;
+    //int height;
+    //float speed2 = 10.0f;
+    void Start()
+    {
+        //width = Screen.width;
+        //height = Screen.height;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButton(0))
+        {
+            if (Input.GetAxis("Mouse X") < 0)
+            {
+                transform.position -= new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed,
+                    0.0f, Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed);
+            }
+
+            else if (Input.GetAxis("Mouse X") > 0)
+            {
+                transform.position -= new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed,
+                    0.0f, Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed);
+            }
+        }
+
+        //if (Input.mousePosition.x < width - boundary)
+        //{
+        //    transform.position -= new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed2,
+        //        0.0f, 0.0f);
+        //}
+
+        //if (Input.mousePosition.x > 0 + boundary)
+        //{
+        //    transform.position -= new Vector3(Input.GetAxisRaw("Mouse X") * Time.deltaTime * speed2,
+        //        0.0f, 0.0f);
+        //}
+
+        //if (Input.mousePosition.y < height - boundary)
+        //{
+        //    transform.position -= new Vector3(0.0f, 0.0f,
+        //        Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed2);
+        //}
+
+        //if (Input.mousePosition.y > 0 + boundary)
+        //{
+        //    transform.position -= new Vector3(0.0f, 0.0f,
+        //        Input.GetAxisRaw("Mouse Y") * Time.deltaTime * speed2);
+        //}
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,105 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Player : NetworkBehaviour
+{
+    [SerializeField] private float shootDistance = 10;
+    private Transform targetedEnemy;
+    private bool enemyClicked;
+    private bool walking;
+    private Animator anim;
+    private UnityEngine.AI.NavMeshAgent navAgent;
+    private float timeBetweenShots = 2f;
+    private float nextFire;
+	// Use this for initialization
+	void Start ()
+	{
+	    anim = GetComponent<Animator>();
+	    navAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+	}
+	
+	// Update is called once per frame
+    void Update()
+    {
+        if (!isLocalPlayer)
+        {
+            return;
+        }
+
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+
+        if (Input.GetButtonDown("Fire2"))
+        {
+            if (Physics.Raycast(ray, out hit, 200))
+            {
+                if (hit.collider.CompareTag("Enemy"))
+                {
+                    targetedEnemy = hit.transform;
+                    enemyClicked = true;
+                }
+                else
+                {
+                    walking = true;
+                    enemyClicked = false;
+                    navAgent.destination = hit.point;
+                    navAgent.Resume();
+                }
+            }
+        }
+
+        if (enemyClicked)
+        {
+            MoveAndShot();
+        }
+
+        if (navAgent.remainingDistance <= navAgent.stoppingDistance)
+        {
+            walking = false;
+        }
+        else
+        {
+            walking = true;
+        }
+
+        //anim.SetBool("IsWalking", walking);
+    }
+
+    void MoveAndShot()
+	{
+	    if (targetedEnemy == null)
+	    {
+	        return;
+	    }
+
+	    navAgent.destination = targetedEnemy.position;
+
+	    if (navAgent.remainingDistance >= shootDistance)
+	    {
+	        navAgent.Resume();
+	        walking = true;
+	    }
+
+	    if (navAgent.remainingDistance <= shootDistance)
+	    {
+	        transform.LookAt(targetedEnemy);
+
+	        if (Time.time > nextFire)
+	        {
+	            nextFire = Time.time + timeBetweenShots;
+	            Fire();
+	        }
+
+	        navAgent.Stop();
+	        walking = false;
+	    }
+	}
+
+    void Fire()
+    {
+        print("FIRE!!!!");
+    }
+
+}
